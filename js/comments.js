62fb87d9d5e6e00e51ab7bf569fc8db9f47dd10a
Vincent Maggioli <Vincent.Maggioli@cerner.com>
2017-07-18 11:07:53 -0500

    Semicolon key blocked

--- js/comments.js
+++ js/comments.js
@@ -5,6 +5,13 @@ function add_criteria() {
   var newChild = document.createElement('div');
   newChild.innerHTML = '<h3>Criteria</h3><select name="criteria'+count+'"><option value=""></option><option value="Guiding">Guiding</option><option value="Low Chair">Low Chair</option><option value="Mark Time">Mark Time</option><option value="High Chair">High Chair</option><option value="Flanks">Flanks</option></select><h3>Change</h3><input name="change'+count+'" type="radio" value="Better">Better<br><input name="change'+count+'" type="radio" value="Worse">Worse<br><h3>Comments (optional)</h3><textarea id="comments'+count+'" name="comments'+count+'" rows="4" cols="25" maxlength="100" onkeydown="updateCharCount(this.name)"></textarea><br><hr>'
   wrapper.appendChild(newChild);
+  addValidateListener(document.getElementById('comments'+count))
   document.getElementById("counter").value = count;
 	count++;
+}
+
+function addValidateListener(elementTextArea) {
+  elementTextArea.addEventListener('keydown',function() {
+    elementTextArea.value = elementTextArea.value.replace(";", "");
+  });
 }
