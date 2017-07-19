3101a825a5f8e4d97337aef036473316001cf664
Colin Milhaupt <colinmilhaupt@gmail.com>
2017-07-19 16:25:48 -0700

    Fix php (#14)
    * Possibly fixed, waiting for DB access to test
    
    * Fixed counter variable line
    
    * Fixed counter update
    
    * Fixed PHP script to work with multiple entries

--- scripts/send.php
+++ scripts/send.php
@@ -1,5 +1,4 @@
 <?php
-
 include("../../config/db_config.php");
 header("Refresh:2; url=http://web.ics.purdue.edu/~cmilhaup/public/auditions.html");
 
@@ -7,35 +6,38 @@ try {
 	$conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-	echo "Connection failed: " . $e->getMessage() . "<br>";
+	echo "Connection failed: " . $e->getMessage() . "</br>";
 }
 
-//works with just 1 entry per person
-$date = date('Y-m-d H:i:s', time());
-$student_leader = $_POST["leader"];
-$student = $_POST["student"];
-$criteria = $_POST['criteria'];
-$difference = $_POST['change'];
-$comments = $_POST["comments"];
- 
-try {
-	$sql = $conn->prepare("INSERT INTO test VALUES (:SL, :STUDENT, :CRITERIA,
-		:DIFFERENCE, :COMMENTS, :DATE)");
-	$sql->bindParam(':SL', $student_leader);
-	$sql->bindParam(':STUDENT', $student);
-	$sql->bindParam(':CRITERIA', $criteria);
-	$sql->bindParam(':DIFFERENCE', $difference);
-	$sql->bindParam(':COMMENTS', $comments);
-	$sql->bindParam(':DATE', $date);
+$count = $_POST["counter"];
 
-    $sql->execute();
-	echo "Records added successfully.";
+for ($i=1; $i<=$count; $i++) {
+	$date = date('Y-m-d H:i:s', time());
+	$student_leader = $_POST["leader"];
+	$student = $_POST["student"];
+	$criteria = $_POST["criteria" . $i];
+	$difference = $_POST["change" . $i];
+	$comments = $_POST["comments" . $i];
 
-} catch(PDOException $e) {
-    echo "ERROR: Could not process request.<br>";
-	echo $e->getMessage();
+	try {
+		$sql = $conn->prepare("INSERT INTO test VALUES (:SL, :STUDENT, :CRITERIA,
+			:DIFFERENCE, :COMMENTS, :DATE)");
+		$sql->bindParam(':SL', $student_leader);
+		$sql->bindParam(':STUDENT', $student);
+		$sql->bindParam(':CRITERIA', $criteria);
+		$sql->bindParam(':DIFFERENCE', $difference);
+		$sql->bindParam(':COMMENTS', $comments);
+		$sql->bindParam(':DATE', $date);
+
+	    $sql->execute();
+		echo "Records added successfully.";
+
+	} catch(PDOException $e) {
+	    echo "ERROR: Could not process request.<br>";
+		echo $e->getMessage();
+	}
 }
- 
+
 // close connection
 $conn = null;
 
