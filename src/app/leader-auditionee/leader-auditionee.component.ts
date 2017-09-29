5b2589c383ab2d709f95c8a18d7ba4054fb04825
Vincent Maggioli <vinmags16@gmail.com>
2017-09-29 14:21:35 -0400

    Implemented basic tests (#24)
    Run tests by executing "ng test".  Tests should be added to components as more features are added.

--- src/app/leader-auditionee/leader-auditionee.component.ts
+++ src/app/leader-auditionee/leader-auditionee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JudgementComponent } from '../judgement/judgement.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
--- src/app/shared/auth.service.spec.ts
+++ src/app/shared/auth.service.spec.ts
@@ -1,11 +1,28 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 import { AuthService } from './auth.service';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 describe('AuthService', () => {
   beforeEach(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAmDhvEdGwMZ6SuZKibrUAVHCpR0DFpnXo",
+      authDomain: "audition-web-form.firebaseapp.com",
+      databaseURL: "https://audition-web-form.firebaseio.com",
+      projectId: "audition-web-form",
+      storageBucket: "audition-web-form.appspot.com",
+      messagingSenderId: "32575069764"
+    }
+
     TestBed.configureTestingModule({
-      providers: [AuthService]
+      providers: [
+        AuthService,
+        AngularFireAuth,
+      ],
+      imports: [ AngularFireModule.initializeApp(firebaseConfig) ]
     });
   });
 
