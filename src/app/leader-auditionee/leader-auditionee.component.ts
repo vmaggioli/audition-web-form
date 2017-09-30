68011e420fba623964361abeccdd9a98a552d4dd
Vincent Maggioli <vinmags16@gmail.com>
2017-09-29 23:54:54 -0400

    SLs can now add a comment at the bottom of the current comment (#31)

--- src/app/leader-auditionee/leader-auditionee.component.ts
+++ src/app/leader-auditionee/leader-auditionee.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, ComponentFactoryResolver, ViewContainerRef, ViewChild, AfterViewInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { JudgementComponent } from '../judgement/judgement.component';
 import { FormsModule } from '@angular/forms';
+import { DynamicModule } from '../dynamic-module';
 
 @Component({
   selector: 'app-leader-auditionee',
   templateUrl: './leader-auditionee.component.html'
 })
 
-export class LeaderAuditioneeComponent {
+@NgModule({
+	declarations: [
+		LeaderAuditioneeComponent,
+		JudgementComponent
+	],
+	imports: [
+		BrowserModule,
+		DynamicModule.withComponents([JudgementComponent])
+	]
+})
 
+export class LeaderAuditioneeComponent implements AfterViewInit {
+	@ViewChild('target', { read: ViewContainerRef }) target: ViewContainerRef;
+	constructor(private cfr: ComponentFactoryResolver) { }
+
+	ngAfterViewInit() {
+		this.putInMyHtml();
+	}
+
+	private putInMyHtml() {
+		let compFactory = this.cfr.resolveComponentFactory(JudgementComponent);
+		this.target.createComponent(compFactory);
+	}
+
+	private submitComment() {
+		// TODO
+	}
 }
