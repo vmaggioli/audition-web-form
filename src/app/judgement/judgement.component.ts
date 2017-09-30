5391410d1dd0f4ced45b6d2cddc2141b3251da30
Vincent Maggioli <vinmags16@gmail.com>
2017-09-30 13:34:46 -0400

    Writing data to Firebase now works (#32)
    NOTE: I changed inputs of drop downs and auto fill boxes to just
    straight in put boxes for now, for proof of concept.
    
    ALSO NOTE: this will definitely be modified in the future as we will be
    changing the layout of the site to Angular Material instead of HTML
    elements.  This was just to lay the writing for all components so they
    can be modified as components change.

--- src/app/judgement/judgement.component.ts
+++ src/app/judgement/judgement.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, AfterViewInit, OnInit, Input } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-judgement',
   templateUrl: 'judgement.component.html'
 })
 
-export class JudgementComponent {
+export class JudgementComponent implements AfterViewInit {
+  private section: string = 'Trumpets';
+  private criteria: string;
+  private comment: string;
+  private goodOrBad: string;
 
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.goodOrBad = '';
+  }
+
+  onKeyCriteria(event : any) {
+    this.criteria = event.target.value;
+  }
+
+  onKeyComment(event : any) {
+    this.comment = event.target.value;
+  }
+
+  public getCriteria() : String { return this.criteria; }
+  public getGoodOrBad() : String { return this.goodOrBad; }
+  public getComment() : String { return this.comment; }
 }
