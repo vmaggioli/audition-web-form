6d318d04b435e96be28e97fb5887aeb74be13022
Kai Hoffman <hoffm154@purdue.edu>
2017-09-30 04:33:05 -0400

    Add textarea to type list of new leaders

--- src/app/leader-auditionee/leader-auditionee.component.ts
+++ src/app/leader-auditionee/leader-auditionee.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { JudgementComponent } from '../judgement/judgement.component';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { Leaders } from './leader';
 
 @Component({
   selector: 'app-leader-auditionee',
@@ -8,5 +11,49 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class LeaderAuditioneeComponent {
+  leaders = STUDENTLEADERS;
+  newLeaders: Leaders[];
 
+  addLeaders() {
+    const rmadd = document.getElementById('rmadd');
+    const div = document.createElement('div');
+    const input = document.createElement('textarea');
+    input.style.height = "150px";
+    input.placeholder = "leader_name_1, leader_name_2, etc.";
+    input.onchange = this.handleTyping;
+    const btn = document.createElement('button');
+    btn.textContent = "Add";
+
+    div.appendChild(input);
+    div.appendChild(btn);
+    rmadd.appendChild(div);
+  }
+
+  handleTyping = (ev) => { // TODO split is too large
+    const list = ev.target.value.split('\n'); // split ==> array
+    this.newLeaders = new Array(list.length);
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].length === 0) {
+        continue;
+      }
+      this.newLeaders.push({ name: list[i] });
+    }
+  }
 }
+
+const STUDENTLEADERS: Leaders[] = [
+  {name: '              '},
+  {name: 'Alex L.'},
+  {name: 'Alex K.'},
+  {name: 'Bobby'},
+  {name: 'Cassie'},
+  {name: 'Colin'},
+  {name: 'Jeremy'},
+  {name: 'JC'},
+  {name: 'Kristin'},
+  {name: 'Nick'},
+  {name: 'Ryan'},
+  {name: 'Tim'},
+  {name: 'Vince'},
+];
+
---
+++ src/app/leader-auditionee/leader.ts
@@ -0,0 +1,3 @@
+export class Leaders {
+  name: string;
+}
