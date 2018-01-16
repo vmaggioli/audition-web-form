58b97e947d119e4709241895dd0356cf040c4dad
Vincent Maggioli <vmaggiol@purdue.edu>
2018-01-16 12:09:20 -0500

    Tab view now works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
-import { LeaderAuditioneeComponent } from './leader-auditionee/leader-auditionee.component';
+import { TabPageComponent } from './tab-page/tab-page.component';
 import { LoginGuard } from './shared/login-guard.module';
 import { SignInErrorComponent } from './error/sign-in-error.component';
 
@@ -10,7 +10,7 @@ const appRoutes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full'},
   { path: 'error', component: SignInErrorComponent},
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'dashboard', component: LeaderAuditioneeComponent, canActivate: [LoginGuard] }
+  { path: 'dashboard', component: TabPageComponent, canActivate: [LoginGuard] }
 ];
 
 @NgModule({
---
+++ src/app/review/review.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-review',
+    templateUrl: './review.component.html'
+})
+
+export class ReviewComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+}
