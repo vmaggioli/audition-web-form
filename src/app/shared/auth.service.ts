822f6583c4806fb9514fb6d17805977cd29b0a7b
Vincent Maggioli <vinmags16@gmail.com>
2018-02-16 13:46:46 -0500

    Added snackbar for invalid login (#72)

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class AuthService {
 
-  loggedIn = false;
+  private loggedIn = false;
 
   constructor(private db: AngularFireDatabase,
-              private router: Router) { }
+              private router: Router,
+              private snackBar: MatSnackBar) { }
 
   login(username: string, password: string) {
     const user: Observable<any> = this.db.object('User').valueChanges();
     user.forEach(data => {
       if (username === data.username && password === data.password) {
-        this.loggedIn = true;
         this.router.navigateByUrl('dashboard');
+        this.loggedIn = true;
       } else {
-        this.router.navigateByUrl('');
+        this.snackBar.open('Invalid login', 'Close', {
+          duration: 3000
+        });
       }
     });
   }
-  
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
 }
