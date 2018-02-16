99fd891fb1eea513b247f97388c319d06b7de424
Vincent Maggioli <vmaggiol@purdue.edu>
2018-02-16 13:05:11 -0500

    Centering and restyling

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,7 +7,8 @@ import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html'
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
 })
 
 export class WelcomeComponent implements OnInit {
