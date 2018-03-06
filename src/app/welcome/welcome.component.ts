b3bcda44b4664b3aeea53c40e7396394f48006a4
Vincent Maggioli <vinmags16@gmail.com>
2018-03-06 13:03:12 -0500

    Removed autodelete, not working (#93) (#94)

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -21,24 +21,7 @@ export class WelcomeComponent implements OnInit {
               private comServ: CommentsService) { }
 
   ngOnInit() {
-    // DB resets at midnight on August 1
-    const now = new Date();
-    const nowUTC = new Date(Date.UTC(now.getFullYear(), now.getMonth()
-    , now.getDate(), now.getHours(), now.getMinutes()));
-    const serv = this.comServ;
-    let waitTime;
-    // Month is 0 indexed in Date
-    if (now.getMonth() >= 8 || (now.getMonth() === 7 && now.getDate() > 1)) {
-      waitTime = new Date(Date.UTC(now.getFullYear() + 1, 7, 1, 12, 0, 0, 0)).getTime() - nowUTC.getTime();
-    } else {
-      waitTime = new Date(Date.UTC(now.getFullYear(), 7, 1, 12, 0, 0, 0)).getTime() - nowUTC.getTime();
-    }
-    if (waitTime > 0) {
-      setTimeout(function () {
-        serv.wipeComments();
-        }, waitTime);
-    }
-  }
+}
 
   login(): void {
     this.auth.login(this.username, this.password);
